8a85e2536409a4b8e101ffd30f84a69d70d76031
kiyoshi sumita <kiyoshi.sumita@madebymira.com>
2019-04-13 20:30:52 +0900

    nRF52 v42136 ・内部RTCによるカレンダー機能実装 ・ECOモード、トリガー処理追加

--- tb_global.h
+++ tb_global.h
@@ -30,6 +30,8 @@
 #include "app_timer.h"
 #include "app_gpiote.h"
 #include "nrf_delay.h"
+#include "nrf_drv_rtc.h"
+#include "nrf_drv_clock.h"
 
 #include "nrf_ble_qwr.h"
 #include "fds.h"
@@ -92,7 +94,7 @@
 //#define APP_FIRMWARE_VERSION_VALUE    0xA0, 0x50 /* v41040 refactoring version */
 //#define APP_FIRMWARE_VERSION_VALUE    0xA0, 0x60 /* v41056 LINE support version */
 //#define APP_FIRMWARE_VERSION_VALUE    0xA0, 0x70 /* v41072 Tangerine Secure Beacon v1.0 support */
-#define APP_FIRMWARE_VERSION_VALUE    0xA4, 0x97 /* v42135 nRF52 Tangerine Secure Beacon v1.1 support */
+#define APP_FIRMWARE_VERSION_VALUE    0xA4, 0x98 /* v42136 nRF52 Tangerine Secure Beacon v1.1 support */
 
 //----------------------------------------------------------------------------
 // device name and company identifier
